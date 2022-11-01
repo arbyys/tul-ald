a5af5ed915da0f2fc490cc594f9d608e0116de3b
arbyys <adam.petricek@centrum.cz>
2022-11-01 23:18:47 +0100

    update

--- cv2/fifo.py
+++ cv2/fifo.py
@@ -1,20 +1,55 @@
+from copy import copy
+import sys
+import string
+
+
 class Queue:
     def __init__(self):
         self.root = None
 
-    def enqueue(self, data):
-        if (self.root == None):
-            self.root = QueueItem(data)
-            return
-        
-        pass
+    def isEmpty(self):
+        return self.root == None
 
-    def dequeue(self):
-        pass
+    def push(self, data):
+        newItem = QueueItem(data)
+        if (self.root == None):
+            self.root = newItem
+            return
+        currentItem = self.root
+        while (currentItem.next):
+            currentItem = currentItem.next
+        currentItem.next = newItem
+        return newItem
+
+    def pop(self):
+        if (self.isEmpty()):
+            return
+        tempReturn = copy(self.root)
+        temp = self.root
+        self.root = temp.next
+        temp = None
+        tempReturn.makeUppercase()
+        return tempReturn
 
 
 class QueueItem:
-    def __init__(self, data):
+    def __init__(self, data=None):
         self.data = data
         self.next = None
-        self.prev = None
+
+    def __str__(self):
+        return self.data
+
+    def makeUppercase(self):
+        self.data = string.capwords(self.data)
+
+
+queue = Queue()
+for line in sys.stdin:
+    line = line.rstrip()
+    queue.push(line)
+
+while (True):
+    if (queue.isEmpty()):
+        break
+    print(queue.pop().data)
---
+++ cv2/lifo.py
@@ -0,0 +1,53 @@
+from copy import copy
+import sys
+import string
+
+
+class Queue:
+    def __init__(self):
+        self.root = None
+
+    def isEmpty(self):
+        return self.root == None
+
+    def push(self, data):
+        newItem = QueueItem(data)
+        if (self.root == None):
+            self.root = newItem
+            return
+        newItem.next = self.root
+        self.root = newItem
+        return newItem
+
+    def pop(self):
+        if (self.isEmpty()):
+            return
+        tempReturn = copy(self.root)
+        temp = self.root
+        self.root = temp.next
+        temp = None
+        tempReturn.makeUppercase()
+        return tempReturn
+
+
+class QueueItem:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        return self.data
+
+    def makeUppercase(self):
+        self.data = string.capwords(self.data)
+
+
+queue = Queue()
+for line in sys.stdin:
+    line = line.rstrip()
+    queue.push(line)
+
+while (True):
+    if (queue.isEmpty()):
+        break
+    print(queue.pop().data)
